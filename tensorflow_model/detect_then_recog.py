f358278c4b082d5be62640d96380c05cdd9cfe55
mukundkhanna123 <50327546+mukundkhanna123@users.noreply.github.com>
2019-05-21 16:12:12 +0530

    Update detect_then_recog.py

--- tensorflow_model/detect_then_recog.py
+++ tensorflow_model/detect_then_recog.py
@@ -13,6 +13,7 @@ Also create the following  directories where this program is stored and provide 
 # -*- coding: utf-8 -*-
 # pylint: disable=C0103
 # pylint: disable=E1101
+
 import time
 import numpy as np
 import tensorflow as tf
@@ -37,7 +38,6 @@ NUM_CLASSES = 2
 label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
 categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
 category_index = label_map_util.create_category_index(categories)
-
 
 class TensoflowFaceDector(object):
 
@@ -91,22 +91,33 @@ class TensoflowFaceDector(object):
         return (boxes, scores, classes, num_detections)
 
 
-if __name__ == "__main__":
-
+def recog(vid_dir):
+    
 
     vid_no = 1
     frame_count = 0
     im_saved = 0
     total_image = 0
-
+    
+    curr_dir = os.getcwd()
     #enter the directory where the videos are stored
     vid_dir = "C:/Users/Mukund/Downloads/Models/tensorflow_model/videos"
 
+    
     #create a temporary directory to hold the images
-    image_dir   = "C:/Users/Mukund/Downloads/Models/tensorflow_model/raw_data"
-
+    if os.path.exists(curr_dir +"/raw_data"):
+        pass
+    else:
+        os.makedirs(curr_dir +"/raw_data")
+    image_dir = curr_dir + "/raw_data"
+    
+    
     #output directory where you want the unique faces to be stored
-    output_dir  = "C:/Users/Mukund/Downloads/Models/tensorflow_model/unique_data"
+    if os.path.exists(curr_dir +"/unique_data"):
+        pass
+    else:
+        os.makedirs(curr_dir +"/unqiue_data")
+    output_dir  = curr_dir + "/unique_data"
     videos = os.listdir(vid_dir)
 
     #uncomment this if you don't want a highlight video of motion detection 
